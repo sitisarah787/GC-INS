b0fe6f80573295f2f23173c4e0d98368fd8c20c8
siti sarah <sitisarh85@gmail.com>
2023-04-12 19:51:21 +0700

    tugas web statis siti sarah

---
+++ Laravel Tugas/resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome</title>
+</head>
+<body>
+    <h1>Welcome, {{ $first_name }} {{ $last_name }}!</h1>
+    <p>Thank you for registering.</p>
+</body>
+</html>
